db40013144ea5e5223f7caaf5a1e507b1d883e73
rebeckao <rebecka.oefverholm@gmail.com>
2022-12-01 21:03:23 +0100

    day 01 part 2

--- src/day_01_calorie_counting.py
+++ src/day_01_calorie_counting.py
@@ -9,3 +9,18 @@ def most_calories_of_single_elf(raw_calories: list[str]) -> int:
         if current_calories > most_calories:
             most_calories = current_calories
     return most_calories
+
+
+def most_calories_of_top_three_elves(raw_calories: list[str]) -> int:
+    elves = []
+    current_calories = 0
+    for food in raw_calories:
+        if not food:
+            elves.append(current_calories)
+            current_calories = 0
+            continue
+        current_calories += int(food)
+    elves.append(current_calories)
+    elves.sort()
+    elves.reverse()
+    return elves[0] + elves[1] + elves[2]
--- test/test_day_01_calorie_counting.py
+++ test/test_day_01_calorie_counting.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from src.day_01_calorie_counting import most_calories_of_single_elf
+from src.day_01_calorie_counting import most_calories_of_single_elf, most_calories_of_top_three_elves
 from src.utils.utils import lines_from_file
 
 
@@ -29,3 +29,28 @@ class TestDay01(TestCase):
         input_data = lines_from_file("day01.txt")
         actual = most_calories_of_single_elf(input_data)
         self.assertEqual(72017, actual)
+
+    def test_most_calories_of_top_three_elves_example(self):
+        input_data = [
+            "1000",
+            "2000",
+            "3000",
+            "",
+            "4000",
+            "",
+            "5000",
+            "6000",
+            "",
+            "7000",
+            "8000",
+            "9000",
+            "",
+            "10000"
+        ]
+        actual = most_calories_of_top_three_elves(input_data)
+        self.assertEqual(45000, actual)
+
+    def test_most_calories_of_top_three_elves_actual(self):
+        input_data = lines_from_file("day01.txt")
+        actual = most_calories_of_top_three_elves(input_data)
+        self.assertEqual(212520, actual)
