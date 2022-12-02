0fa5cb11346cab7ad3cee11844f27ceb278ecccd
rebeckao <rebecka.oefverholm@gmail.com>
2022-12-02 23:00:16 +0100

    day 02 part 2

--- src/day_02_rock_paper_scissor.py
+++ src/day_02_rock_paper_scissor.py
@@ -46,3 +46,50 @@ def score(instruction: str) -> int:
 
 def score_after_instructions(raw_instructions: list[str]) -> int:
     return sum([score(instruction) for instruction in raw_instructions])
+
+
+class Outcome:
+    LOSE = "X"
+    DRAW = "Y"
+    WIN = "Z"
+
+
+def score_given_outcome(instruction: str) -> int:
+    their_play, desired_outcome = instruction.split(" ")
+    match their_play:
+        case Play.THEIR_ROCK:
+            match desired_outcome:
+                case Outcome.LOSE:
+                    return 3 + 0
+                case Outcome.DRAW:
+                    return 1 + 3
+                case Outcome.WIN:
+                    return 2 + 6
+                case _:
+                    raise Exception("Not ok play from you: " + desired_outcome)
+        case Play.THEIR_PAPER:
+            match desired_outcome:
+                case Outcome.LOSE:
+                    return 1 + 0
+                case Outcome.DRAW:
+                    return 2 + 3
+                case Outcome.WIN:
+                    return 3 + 6
+                case _:
+                    raise Exception("Not ok play from you: " + desired_outcome)
+        case Play.THEIR_SCISSORS:
+            match desired_outcome:
+                case Outcome.LOSE:
+                    return 2 + 0
+                case Outcome.DRAW:
+                    return 3 + 3
+                case Outcome.WIN:
+                    return 1 + 6
+                case _:
+                    raise Exception("Not ok play from you: " + desired_outcome)
+        case _:
+            raise Exception("Not ok play from them: " + their_play)
+
+
+def score_after_instructions_with_outcomes(raw_instructions: list[str]) -> int:
+    return sum([score_given_outcome(instruction) for instruction in raw_instructions])
--- test/test_day_02_rock_paper_scissor.py
+++ test/test_day_02_rock_paper_scissor.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from src.day_02_rock_paper_scissor import score_after_instructions
+from src.day_02_rock_paper_scissor import score_after_instructions, score_after_instructions_with_outcomes
 from src.utils.utils import lines_from_file
 
 
@@ -18,3 +18,17 @@ class TestRockPaperScissor(TestCase):
         input_data = lines_from_file("day02.txt")
         actual = score_after_instructions(input_data)
         self.assertEqual(12740, actual)
+
+    def test_score_after_instructions_with_outcomes_example(self):
+        input_data = [
+            "A Y",
+            "B X",
+            "C Z"
+        ]
+        actual = score_after_instructions_with_outcomes(input_data)
+        self.assertEqual(12, actual)
+
+    def test_score_after_instructions_with_outcomes_actual(self):
+        input_data = lines_from_file("day02.txt")
+        actual = score_after_instructions_with_outcomes(input_data)
+        self.assertEqual(11980, actual)
