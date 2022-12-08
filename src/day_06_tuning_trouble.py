6ad4cc46f833cbce1b9a45ed52c88a883f38449e
rebeckao <rebecka.oefverholm@gmail.com>
2022-12-08 21:51:13 +0100

    day 06 part 2

--- src/day_06_tuning_trouble.py
+++ src/day_06_tuning_trouble.py
@@ -1,6 +1,5 @@
-def characters_before_start_marker(datastream_buffer: str) -> 0:
+def characters_before_start_marker(datastream_buffer: str, marker_size: int) -> 0:
     for i in range(3, len(datastream_buffer) + 1):
-        letter = datastream_buffer[i]
-        if len({datastream_buffer[i - 1], datastream_buffer[i - 2], datastream_buffer[i - 3], letter}) == 4:
+        if len(set(datastream_buffer[i - (marker_size - 1):i + 1])) == marker_size:
             return i + 1
     return 0
--- test/test_day_06_tuning_trouble.py
+++ test/test_day_06_tuning_trouble.py
@@ -5,12 +5,22 @@ from src.utils.utils import lines_from_file
 
 
 class Test(TestCase):
-    def test_characters_before_start_marker_example(self):
-        self.assertEqual(7, characters_before_start_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
-        self.assertEqual(5, characters_before_start_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"))
-        self.assertEqual(6, characters_before_start_marker("nppdvjthqldpwncqszvftbrmjlhg"))
-        self.assertEqual(10, characters_before_start_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
-        self.assertEqual(11, characters_before_start_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+    def test_characters_before_start_of_packet__marker_example(self):
+        self.assertEqual(7, characters_before_start_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4))
+        self.assertEqual(5, characters_before_start_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4))
+        self.assertEqual(6, characters_before_start_marker("nppdvjthqldpwncqszvftbrmjlhg", 4))
+        self.assertEqual(10, characters_before_start_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4))
+        self.assertEqual(11, characters_before_start_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4))
 
-    def test_characters_before_start_marker_actual(self):
-        self.assertEqual(1287, characters_before_start_marker(lines_from_file("day06.txt")[0]))
+    def test_characters_before_start_of_packet__marker_actual(self):
+        self.assertEqual(1287, characters_before_start_marker(lines_from_file("day06.txt")[0], 4))
+
+    def test_characters_before_start_of_message_marker_example(self):
+        self.assertEqual(19, characters_before_start_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14))
+        self.assertEqual(23, characters_before_start_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14))
+        self.assertEqual(23, characters_before_start_marker("nppdvjthqldpwncqszvftbrmjlhg", 14))
+        self.assertEqual(29, characters_before_start_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14))
+        self.assertEqual(26, characters_before_start_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14))
+
+    def test_characters_before_start_of_message_marker_actual(self):
+        self.assertEqual(3716, characters_before_start_marker(lines_from_file("day06.txt")[0], 14))
