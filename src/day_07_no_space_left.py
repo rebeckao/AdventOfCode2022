56cd8991f4e7317669a29c20fb4f9b51da6cf9ff
rebeckao <rebecka.oefverholm@gmail.com>
2022-12-10 20:17:02 +0100

    day 07 part 2

--- src/day_07_no_space_left.py
+++ src/day_07_no_space_left.py
@@ -24,7 +24,7 @@ class File:
 
 
 def sum_of_small_directory_sizes(raw_input: list[str]) -> int:
-    all_directories = parse_file_system(raw_input)
+    all_directories, _ = parse_file_system(raw_input)
     sum_of_sizes = 0
     for directory in all_directories:
         size = directory.total_size()
@@ -33,7 +33,19 @@ def sum_of_small_directory_sizes(raw_input: list[str]) -> int:
     return sum_of_sizes
 
 
-def parse_file_system(raw_input) -> list[Directory]:
+def size_of_directory_to_delete(raw_input: list[str]) -> int:
+    all_directories, root_size = parse_file_system(raw_input)
+    free_space = 70000000 - root_size
+    required = 30000000 - free_space
+    smallest_candidate = 70000000
+    for directory in all_directories:
+        size = directory.total_size()
+        if required <= size < smallest_candidate:
+            smallest_candidate = size
+    return smallest_candidate
+
+
+def parse_file_system(raw_input) -> (list[Directory], int):
     root = Directory(None, dict(), "/")
     all_directories = [root]
     current_directory = root
@@ -56,4 +68,4 @@ def parse_file_system(raw_input) -> list[Directory]:
             else:
                 file_size, name = line.split(" ")
                 current_directory.children[name] = File(int(file_size))
-    return all_directories
+    return all_directories, root.total_size()
--- test/test_day_07_no_space_left.py
+++ test/test_day_07_no_space_left.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from src.day_07_no_space_left import sum_of_small_directory_sizes
+from src.day_07_no_space_left import sum_of_small_directory_sizes, size_of_directory_to_delete
 from src.utils.utils import lines_from_file
 
 
@@ -67,3 +67,37 @@ class Test(TestCase):
         input_data = lines_from_file("day07.txt")
         actual = sum_of_small_directory_sizes(input_data)
         self.assertEqual(1423358, actual)
+
+    def test_size_of_directory_to_delete_example(self):
+        input_data = [
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "dir d",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+            "$ cd d",
+            "$ ls",
+            "4060174 j",
+            "8033020 d.log",
+            "5626152 d.ext",
+            "7214296 k",
+        ]
+        actual = size_of_directory_to_delete(input_data)
+        self.assertEqual(24933642, actual)
+
+    def test_size_of_directory_to_delete_actual(self):
+        input_data = lines_from_file("day07.txt")
+        actual = size_of_directory_to_delete(input_data)
+        self.assertEqual(545729, actual)
