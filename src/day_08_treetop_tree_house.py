351c673bc648c8809833806865774d55d9f27bbf
rebeckao <rebecka.oefverholm@gmail.com>
2022-12-11 16:22:27 +0100

    day 08 part 2

--- src/day_08_treetop_tree_house.py
+++ src/day_08_treetop_tree_house.py
@@ -30,3 +30,32 @@ def visible_trees(raw_lines: list[str]) -> int:
 
     return sum([sum(visible_tree) for visible_tree in visibilities])
 
+
+def highest_scenic_score(raw_lines: list[str]) -> int:
+    max_y = len(raw_lines)
+    max_x = len(raw_lines[0])
+    max_scenic_score = 0
+    for y in range(0, max_y):
+        for x in range(0, max_x):
+            current_height = int(raw_lines[y][x])
+            seen_left, seen_right, seen_above, seen_below = 0, 0, 0, 0
+            for left_x in reversed(range(0, x)):
+                seen_left += 1
+                if int(raw_lines[y][left_x]) >= current_height:
+                    break
+            for right_x in range(x + 1, max_x):
+                seen_right += 1
+                if int(raw_lines[y][right_x]) >= current_height:
+                    break
+            for above_y in reversed(range(0, y)):
+                seen_above += 1
+                if int(raw_lines[above_y][x]) >= current_height:
+                    break
+            for below_y in range(y + 1, max_y):
+                seen_below += 1
+                if int(raw_lines[below_y][x]) >= current_height:
+                    break
+            scenic_score = seen_left * seen_right * seen_above * seen_below
+            if scenic_score > max_scenic_score:
+                max_scenic_score = scenic_score
+    return max_scenic_score
--- test/test_day_08_treetop_tree_house.py
+++ test/test_day_08_treetop_tree_house.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from src.day_08_treetop_tree_house import visible_trees
+from src.day_08_treetop_tree_house import visible_trees, highest_scenic_score
 from src.utils.utils import lines_from_file
 
 
@@ -20,3 +20,19 @@ class Test(TestCase):
         input_data = lines_from_file("day08.txt")
         actual = visible_trees(input_data)
         self.assertEqual(1792, actual)
+
+    def test_highest_scenic_score_example(self):
+        input_data = [
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390",
+        ]
+        actual = highest_scenic_score(input_data)
+        self.assertEqual(8, actual)
+
+    def test_highest_scenic_score_actual(self):
+        input_data = lines_from_file("day08.txt")
+        actual = highest_scenic_score(input_data)
+        self.assertEqual(334880, actual)
