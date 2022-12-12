b80eb7477fdff482c3e8d56efaf99c2afd86b68f
rebeckao <rebecka.oefverholm@gmail.com>
2022-12-12 22:25:52 +0100

    day 09 part 2

--- src/day_09_rope_bridge.py
+++ src/day_09_rope_bridge.py
@@ -32,3 +32,48 @@ def positions_tail_visited(raw_lines: list[str]) -> 0:
                     tail_y -= 1
             visited.add(str(tail_x) + "_" + str(tail_y))
     return len(visited)
+
+
+def positions_tail_visited_for_longer_rope(raw_lines: list[str]) -> 0:
+    knot_xs = [0 for _ in range(0, 10)]
+    knot_ys = [0 for _ in range(0, 10)]
+    visited = set()
+    visited.add("0_0")
+    for line in raw_lines:
+        direction, raw_steps = line.split(" ")
+        steps = int(raw_steps)
+        for _ in range(0, steps):
+            # head
+            if direction == "R":
+                knot_xs[0] += 1
+            elif direction == "L":
+                knot_xs[0] -= 1
+            elif direction == "D":
+                knot_ys[0] += 1
+            elif direction == "U":
+                knot_ys[0] -= 1
+            # tails
+            for knot_index in range(1, 10):
+                new_x, new_y = move_knot(knot_xs[knot_index - 1], knot_ys[knot_index - 1], knot_xs[knot_index], knot_ys[knot_index])
+                knot_xs[knot_index] = new_x
+                knot_ys[knot_index] = new_y
+            visited.add(str(knot_xs[9]) + "_" + str(knot_ys[9]))
+    return len(visited)
+
+
+def move_knot(head_x, head_y, tail_x, tail_y) -> (int, int):
+    x_diff = head_x - tail_x
+    y_diff = head_y - tail_y
+    if abs(x_diff) >= 1 and abs(y_diff) >= 1 and (abs(x_diff) > 1 or abs(y_diff) > 1):
+        tail_x += int(x_diff / abs(x_diff))
+        tail_y += int(y_diff / abs(y_diff))
+    else:
+        if head_x > tail_x + 1:
+            tail_x += 1
+        elif head_x < tail_x - 1:
+            tail_x -= 1
+        if head_y > tail_y + 1:
+            tail_y += 1
+        elif head_y < tail_y - 1:
+            tail_y -= 1
+    return tail_x, tail_y
--- test/test_day_09_rope_bridge.py
+++ test/test_day_09_rope_bridge.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from src.day_09_rope_bridge import positions_tail_visited
+from src.day_09_rope_bridge import positions_tail_visited, positions_tail_visited_for_longer_rope
 from src.utils.utils import lines_from_file
 
 
@@ -23,3 +23,36 @@ class Test(TestCase):
         input_data = lines_from_file("day09.txt")
         actual = positions_tail_visited(input_data)
         self.assertEqual(6057, actual)
+
+    def test_positions_tail_visited_for_longer_rope_short_example(self):
+        input_data = [
+            "R 4",
+            "U 4",
+            "L 3",
+            "D 1",
+            "R 4",
+            "D 1",
+            "L 5",
+            "R 2",
+        ]
+        actual = positions_tail_visited_for_longer_rope(input_data)
+        self.assertEqual(1, actual)
+
+    def test_positions_tail_visited_for_longer_rope_large_example(self):
+        input_data = [
+            "R 5",
+            "U 8",
+            "L 8",
+            "D 3",
+            "R 17",
+            "D 10",
+            "L 25",
+            "U 20",
+        ]
+        actual = positions_tail_visited_for_longer_rope(input_data)
+        self.assertEqual(36, actual)
+
+    def test_positions_tail_visited_for_longer_rope_actual(self):
+        input_data = lines_from_file("day09.txt")
+        actual = positions_tail_visited_for_longer_rope(input_data)
+        self.assertEqual(2514, actual)
