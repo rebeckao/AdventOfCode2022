46e4d305c75f4ac465a152e91ed86a490e1be013
rebeckao <rebecka.oefverholm@gmail.com>
2022-12-15 21:00:21 +0100

    day 10 part 2

--- src/day_10_cathode_ray_tube.py
+++ src/day_10_cathode_ray_tube.py
@@ -16,3 +16,42 @@ def sum_of_signal_strengths(raw_lines: list[str]) -> int:
             signal_strength_sum += start_x * cycle_to_check
             cycle_to_check += 40
     return signal_strength_sum
+
+
+def visualize_signal(raw_lines: list[str]) -> list[str]:
+    output = [""]
+    current_output_line = 0
+    cycle = 1
+    x = 1
+    for index, line in enumerate(raw_lines):
+        current_cycle = cycle
+        start_x = x
+
+        if len(output[current_output_line]) == 40:
+            current_output_line += 1
+            output.append("")
+        current_position_on_line = (current_cycle - current_output_line * 40) - 1
+        if abs(start_x - current_position_on_line) < 2:
+            output[current_output_line] += "#"
+        else:
+            output[current_output_line] += "."
+
+        if line == "noop":
+            cycle = current_cycle + 1
+        else:
+            current_cycle += 1
+            if len(output[current_output_line]) == 40:
+                current_output_line += 1
+                output.append("")
+            current_position_on_line = (current_cycle - current_output_line * 40) - 1
+            if abs(start_x - current_position_on_line) < 2:
+                output[current_output_line] += "#"
+            else:
+                output[current_output_line] += "."
+
+            cycle = current_cycle + 1
+            _, value = line.split(" ")
+            x = x + int(value)
+    for line in output:
+        print(line)
+    return output
